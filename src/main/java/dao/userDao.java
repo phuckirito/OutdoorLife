127fd4c0d01f648046cbe3da3941807cee3ab4ee
ADMIN <ADMIN@ACER>
2024-07-04 17:12:34 +0700

    fix bug

--- src/main/java/dao/userDao.java
+++
@@ -1,6 +0,0 @@
-package dao;
-import model.*;
-public interface UserDAO {
-    boolean insertGuest(String firstName, String lastName, String phoneNumber, String email, String passwordHash);
-    User findByEmail(String email);
-}
